5cf908e926e75a51f53ffb249f53c2c055891c9f
John Christy <christyjohn14@gmail.com>
2023-11-08 19:45:59 -0500

    minor update attempting to start stick()

--- dough.gd
+++ dough.gd
@@ -8,15 +8,15 @@ var scaleSpeed = 1
 var maxScale = 5
 
 func _physics_process(delta):
+	var collision = move_and_collide(velocity * delta)
+	
 	if !hasHit:
-		var collision = move_and_collide(velocity * delta)
-
 		if collision:
 			hasHit = true
 		else:
 			_fall(delta)
 	else:
-		stick()
+		stick(collision)
 		grow(delta)
 
 func initialize(player_position, target_position, camera_dir):
@@ -25,13 +25,12 @@ func initialize(player_position, target_position, camera_dir):
 	var vel_dir: Vector3 = camera_dir.normalized()
 	velocity = velocity.move_toward(vel_dir * throw_speed * vel_dir.length(), 100)
 	
-	
 func _fall(delta):
 	grav_vel = Vector3.ZERO if is_on_floor() else grav_vel.move_toward(Vector3(0, velocity.y - gravity, 0), gravity * delta)
 	velocity = velocity - abs(grav_vel)
 	
-func stick():
-	pass
+func stick(collision):
+	print(collision.get_collider().velocity)
 
 func grow(delta):
 	scale = Vector3(scale.x + scaleSpeed * delta, scale.y + scaleSpeed * delta, scale.z + scaleSpeed * delta)

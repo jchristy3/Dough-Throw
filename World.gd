b221c072f0b13f54435e836a597c7a21e28a6313
John Christy <christyjohn14@gmail.com>
2023-11-18 15:21:55 -0500

    wired dough to world for dough creation signal

--- World.gd
+++ World.gd
@@ -4,5 +4,9 @@ extends Node3D
 
 func _on_player_shoot_dough(player_position, collision_point, camera_direction):
 	var dough = dough_scene.instantiate()
+	dough.connect("create_stuck_dough", _on_dough_scene_create_stuck_dough)
 	dough.initialize(player_position, collision_point, camera_direction)
 	add_child(dough)
+	
+func _on_dough_scene_create_stuck_dough(position: Vector3, body: Node3D):
+	print("SUCCESS")
--- dough.gd
+++ dough.gd
@@ -1,5 +1,7 @@
 extends RigidBody3D
 
+signal create_stuck_dough(position: Vector3, body: Node3D)
+
 var grav_vel: Vector3
 var throw_speed = 2000
 var gravity: float = 3
@@ -9,6 +11,8 @@ var maxScale = 5
 var force: Vector3 = Vector3(0,0,0);
 var stuck = false
 var growing = false
+var at_max_scale = false
+var bodies: Array[Node3D] = []
 
 @onready var joint: HingeJoint3D = $PinJoint3D
 @onready var collisionShape: CollisionShape3D = $CollisionShape3D
@@ -16,10 +20,13 @@ var growing = false
 
 func _physics_process(delta):
 	if !stuck:
-		stick()
+		bodies = get_colliding_bodies()
+		stick(bodies)
 	
-	if growing:
+	if !at_max_scale:
 		grow(delta)
+	else:
+		convert_dough(bodies)
 	
 func initialize(player_position, target_position, camera_direction):
 	var start_position: Vector3 = Vector3(player_position.x, player_position.y, player_position.z)
@@ -28,9 +35,7 @@ func initialize(player_position, target_position, camera_direction):
 	force = camera_direction.normalized() * throw_speed
 	apply_force(force)
 	
-func stick():
-	var bodies = get_colliding_bodies()
-	
+func stick(bodies):
 	for i in bodies:
 		var bName = i.get_name()
 		if bName == "Floor" || bName == "Dough" || bName == "Enemy" || bName.contains("Pillar"):
@@ -38,6 +43,7 @@ func stick():
 			joint.node_b = NodePath(i.get_path())
 			growing = true
 			sleeping = true
+			stuck = true
 	
 func grow(delta):
 	var sphere: SphereShape3D = SphereShape3D.new()
@@ -45,4 +51,13 @@ func grow(delta):
 	collisionShape.scale = collisionShape.scale.clamp(scale, Vector3(maxScale, maxScale, maxScale))
 	meshInstance.scale = Vector3(meshInstance.scale.x + scaleSpeed * delta, meshInstance.scale.y + scaleSpeed * delta, meshInstance.scale.z + scaleSpeed * delta)
 	meshInstance.scale = meshInstance.scale.clamp(scale, Vector3(maxScale, maxScale, maxScale))
+	if meshInstance.scale == Vector3(maxScale, maxScale, maxScale):
+		at_max_scale = true
+		
+func convert_dough(bodies: Array[Node3D]):
+	print(bodies)
+	create_stuck_dough.emit(position, bodies[0])
+	queue_free()
+		
+
 	
